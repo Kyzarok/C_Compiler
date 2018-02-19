f22e2bfaadec868d173d6659d1e20ec457048c8c
Edward A Pickup <edp16@eews304-009.ee.ic.ac.uk>
2018-02-19 16:26:46 +0000

    Added template class for AST

---
+++ src/AST/ast.hpp
@@ -0,0 +1,16 @@
+#ifndef ast_root_hpp
+#define ast_root_hpp
+
+
+class Node //template function, contains only virtual functions to overwrite.
+
+
+{
+public:
+	virtual ~Node (){} //destructor, may or may not be implemented in subclasses
+
+
+};
+
+
+#endif
