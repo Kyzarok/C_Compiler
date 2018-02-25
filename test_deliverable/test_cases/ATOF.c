d041375ca0035deeb012989db8e07760172e4c61
Barrometer <pickup.edward@gmail.com>
2018-02-25 13:44:28 +0000

    Wrote test 10, and emptyish files for other two tests. Unsure how to test for MAIN

---
+++ test_deliverable/test_cases/ATOF.c
@@ -0,0 +1,9 @@
+//Required Test, meets ATOF
+
+double atof(const char* str);
+
+int f(){
+	char demo[4] ="100";
+	double value = atof(demo);
+	return value;
+} 
---
+++ test_deliverable/test_cases/MAIN_driver.c
@@ -0,0 +1,8 @@
+//Driver for required Test MAIN.c
+//Test not implemented
+
+int f();
+
+int main(){
+	return f()!=5; // function under test, f, should return 5. So if it does, return 0, else 1
+}
