3e5e4ae3d0bf418ba796f41b428ca21a4394da68
Barrometer <pickup.edward@gmail.com>
2018-02-25 12:42:30 +0000

    Test case 7

---
+++ test_deliverable/test_cases/SELF_RECURSION.c
@@ -0,0 +1,10 @@
+int f(int x){
+	x--;
+	if(x>1){
+		return(f(x));
+	}
+	else{
+		return 5;
+	}
+
+}
---
+++ test_deliverable/test_cases/SELF_RECURSION_driver.c
@@ -0,0 +1,8 @@
+//driver for test cast number 7, self recursion
+
+int f(int x);
+
+int main(){
+
+	return f(3)!=5; // recursive call of x should always return 5. If so return a zero, else a one
+}
