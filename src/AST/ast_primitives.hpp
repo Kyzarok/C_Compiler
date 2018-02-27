0f9f6acf87426093ec4c0c3d3735a285d1296ef9
Barrometer <33041944+Barrometer@users.noreply.github.com>
2018-02-27 16:58:45 +0000

    Added new ast part - primitives
    Contains starting point for an integer class, based off of DT's example from labs. Need to implement more functions to it (not all from labs used)

---
+++ src/AST/ast_primitives.hpp
@@ -0,0 +1,24 @@
+#ifndef ast_primitives_hpp
+#define ast_primitives_hpp
+
+#include "ast_node.hpp"
+
+class Integer : public Node // an integer
+
+{
+	private:
+		int value; // the value of the integer
+	
+	public:
+	Number(int _value) // constructor with initialiser
+		: value(_value)
+		{}
+	double getValue() const{ // getter
+		return value;
+	}	
+	
+}
+
+//TODO Add more primitives, eg char, string literal, variables. I think they go here?
+
+#endif
