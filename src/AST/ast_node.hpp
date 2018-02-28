ec73f698045a4deb11f9c9c18588da3650d2893a
Edward Pickup <pickup.edward@gmail.com>
2018-02-28 10:56:55 +0000

    Added virtual functions to src/AST/ast_node.hpp

--- src/AST/ast_node.hpp
+++ src/AST/ast_node.hpp
@@ -10,13 +10,29 @@ class Node //template function, contains only virtual functions to overwrite.
 
 
 
-typedef const Node *NodePtr;
+typedef const Node *NodePtr; // use NodePtr to point to other nodes
 
 class Node
 {
 public:
+	
 	virtual ~Node (){} //destructor, may or may not be implemented in subclasses
-	{ throw std::runtime_erro("Node not implemented"); }
+	
+	//copied the following functions from DT's examples, will need work in other places too
+	
+	virtual void print(std::ostream &dst) const =0;
+
+    //! Evaluate the tree using the given mapping of variables to numbers
+    virtual double evaluate(
+        const std::map<std::string,double> &bindings
+    ) const
+    { throw std::runtime_error("Not implemented."); }
+    
+    /* Additional functions to add
+    virtual const translate(std::ostream &dst) const=0; // function to be overwritten by subclasses. Will output translated python code over stdout - send to output file.
+    
+    virtual const compile (std::ostream &dst) const =0; // function to be overwritten by subclasses. Will output compiled MIPS assembly over stdout - send to output file.
+    */
 };
 
 //so top level of abstraction, still kinda confused how this works as surely we need to sort other stuff out before cramming things in here, as in the return types in .y file.
