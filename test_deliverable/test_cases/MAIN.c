46a6e8d826f64d565514f7e35783461b94b6f9ad
Barrometer <pickup.edward@gmail.com>
2018-03-04 13:49:35 +0000

    Test case 9 test and driver

--- test_deliverable/test_cases/MAIN.c
+++ test_deliverable/test_cases/MAIN.c
@@ -1,1 +1,11 @@
-//unsure how to test
+//test no.9
+
+inf f();
+
+int main(){
+
+  int x = f();
+
+  return x; // should return 0 if function call worked
+
+}
--- test_deliverable/test_cases/MAIN_driver.c
+++ test_deliverable/test_cases/MAIN_driver.c
@@ -1,8 +1,6 @@
 //Driver for required Test MAIN.c
 //Test not implemented
 
-int f();
-
-int main(){
-	return f()!=5; // function under test, f, should return 5. So if it does, return 0, else 1
+int f(){
+	return 0;
 }
