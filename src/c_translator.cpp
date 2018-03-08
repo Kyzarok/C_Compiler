79d88af285936a590423af987b91b53f360ab862
Barrometer <pickup.edward@gmail.com>
2018-03-08 16:57:04 +0000

    Translator skeleton is happy

--- src/c_translator.cpp
+++ src/c_translator.cpp
@@ -1,9 +1,19 @@
 //This file will eventually contain the python functionality.
 
 #include"ast.hpp"
+#include <iostream>
+#include<string>
+#include<sstream>
 
-int main(/*Paramaters */){
-
-	// do the thing
+int main(){
+	std::stringstream ss;
+	ss<<"# Boilerplate"<<std::endl<<"if __name__ == \" __main__ \":"<<std::endl;
+	ss<<"import sys"<<std::endl<<"ret=main()"<<std::endl<<"sys.exit(ret)"<<std::endl;
+	const Node *ast=parseAST(); //This function won't really return anything
+	ast->translate(std::cout);
+	std::cout<<std::endl;
+	
+	//now for boilerplate
+	std::cout<<ss.str()<<endl;
 	return 0;
 }
