7f9e959931f8f7331e8344c0ff4b9ebf7d0d3deb
Edward Pickup <pickup.edward@gmail.com>
2018-03-09 17:09:11 +0000

    Begun on framework for file i/o format

--- src/c_compiler.cpp
+++ src/c_compiler.cpp
@@ -1,19 +1,27 @@
-//This file will eventually contain the compile functionality.
+//This file contains the I/O handling for compiler and translator
 
-#include"ast.hpp"
+#include"ast.hpp" //header pointing to various AST nodes
 #include <iostream>
 #include<string>
-#include<sstream>
+#include<sstream> // makes printing boiler plate somewhat quicker
+#include<cstdlib> //Required for exit
 
-int main(/*Paramaters */){
-	//TODO take input parameters, work out input file, target file, and whether compiling or translating (or printing I guess)
+int main(int argc, char *argv[]){
 	
+	//check for expected number of inputs
+	//program should be ran in form location {mode} {source} "-o" {dest}
+	if(argc<5){ //expected number
+		std::cerr<<"ERROR: Incorrect number of arguments provided"<<std::endl;
+		std::exit(1);
+	}
+	// get mode
+	std::string mode_select(argv[1]);
 	const Node *ast=parseAST(); // build the ast
 	//if translator
-	if(true){ // hard code for now, based off of paramaters
+	if(mode_select =="--translate"){ //ie translator mode
 	
 		
-		std::stringstream ss;
+		std::stringstream ss; // make a function
 		ss<<"# Boilerplate"<<std::endl<<"if __name__ == \" __main__ \":"<<std::endl;
 		ss<<"import sys"<<std::endl<<"ret=main()"<<std::endl<<"sys.exit(ret)"<<std::endl;
 		
@@ -25,6 +33,14 @@ int main(/*Paramaters */){
 		std::cerr<<"Boilerplate"<<std::endl;
 		//now for boilerplate
 		std::cout<<ss.str()<<std::endl;
+	}
+	else if(mode_select =="-S"){ //ie compile
+		std::cerr<<"ERROR: Compile unimplemented at time"<<std::endl;
+	}
+	
+	else{
+		std::cerr<<"ERROR: Invalid command"<<std::endl;
+		std::exit(1);
 	}
 	
 	return 0;
