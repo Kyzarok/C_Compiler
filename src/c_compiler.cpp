c9ad744b04fb91eaee0f787edc21a8f2996178cb
Edward Pickup <pickup.edward@gmail.com>
2018-03-12 18:48:23 +0000

    Now outputs to specified file for translate. Need to make it read from specified input

--- src/c_compiler.cpp
+++ src/c_compiler.cpp
@@ -5,6 +5,7 @@
 #include<string>
 #include<sstream> // makes printing boiler plate somewhat quicker
 #include<cstdlib> //Required for exit
+#include<fstream> 
 
 std::string make_boilerplate(){
 	std::stringstream ss; // make a function
@@ -16,11 +17,31 @@ std::string make_boilerplate(){
 int main(int argc, char *argv[]){
 	
 	//check for expected number of inputs
-	//program should be ran in form location {mode} {source} "-o" {dest}
+	//program should be ran in form {location} {mode} {source} "-o" {dest}
 	if(argc<5){ //expected number
 		std::cerr<<"ERROR: Incorrect number of arguments provided"<<std::endl;
 		std::exit(1);
 	}
+	
+	// open source file
+	std::ifstream fileSource;
+	
+	fileSource.open(argv[2]); //the first parameter will be name / location of bin
+	if(!(fileSource.is_open())){ //if not opened then return error
+		std::cerr<<"Source File "<<argv[2]<< " not found"<<std::endl; 
+		std::exit(1);//exit
+	}
+	
+	//open dest file
+	
+	std::ofstream fileDest;
+	
+	fileDest.open(argv[4]); //the location of the dest file
+	if(!(fileDest.is_open())){ //if not opened then return error
+		std::cerr<<"Dest File "<<argv[2]<< " not found"<<std::endl; 
+		std::exit(1);//exit
+	}
+	
 	// get mode
 	std::string mode_select(argv[1]);
 	
@@ -36,11 +57,15 @@ int main(int argc, char *argv[]){
 			
 		std::cerr<<"We translating"<<std::endl;
 		//translate
-		ast->translate(std::cout);
-		std::cout<<std::endl; //TODO make this write to dest file, not stdout. Can put stdout into file stream?
+		
+		ast->translate(fileDest);
+		//fileDest<<std::cout<<std::endl; //TODO make this write to dest file, not stdout. Can put stdout into file stream?
+		
+		
+		
 		std::cerr<<"Boilerplate"<<std::endl;
 		//now for boilerplate
-		std::cout<<make_boilerplate()<<std::endl;
+		fileDest<<make_boilerplate()<<std::endl;
 	}
 	else if(mode_select =="-S"){ //ie compile
 		std::cerr<<"ERROR: Compile unimplemented at time"<<std::endl;
