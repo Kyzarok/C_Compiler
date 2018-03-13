40c17ac28d782f0656afd1e4bffe4a4b71a221ba
Barrometer <pickup.edward@gmail.com>
2018-03-13 15:33:10 +0000

    More work on declarations. Declaration class now exists, now to make declaration list class

--- src/AST/ast_varb_declarations.hpp
+++ src/AST/ast_varb_declarations.hpp
@@ -2,17 +2,51 @@
 #define ast_varb_declarations_hpp
 
 #include "ast_node.hpp"
+#include "ast_expressions.hpp"
+#include <string>
 
-class Declaration : public Declaration{
+class Declaration : public Node{
 
 	protected: 
 		std::string type;
-		std::var_id;
-		
-
+		std::string var_id;
+		ExpressionPtr value;
+	
+	public:
+		Declaration(std::string _type, std::string _var_id) : //constructor with no value assignment
+			type(_type),
+			var_id(_var_id),
+			value(NULL)
+			{std::cerr<<"New Declaration with no value assigned"<<std::endl;}
+		Declaration(std::string _type, std::string _var_id, ExpressionPtr _value) : //constructor with variable assignment
+			type(_type),
+			var_id(_var_id),
+			value(_value)
+			{std::cerr<<"New Declaration with value assigned"<<std::endl;}
+			
+		virtual void print(std::ostream &dst) const override {
+			dst<<type;
+			dst << " ";
+			dst << var_id;
+			dst<<" ";
+			if(value!=NULL){
+				dst<<"= ";
+				value->print(dst);
+			}
+		}
+		virtual void translate(std::ostream &dst, int indent) const override {
+			dst<<var_id;
+			dst<<" = ";
+			if(value!=NULL){
+				value ->translate(dst,indent);
+			}
+			else{
+				dst<<"0";
+			}
+		}
 };
 
-class DeclList;
+/*class DeclList;
 
 typedef const DeclListt *DeclListPtr;
 
@@ -24,7 +58,7 @@ class DeclList : public Node{
 	
 	public:
 };
-
+*/
 
 
 
