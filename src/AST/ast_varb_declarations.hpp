89f0e6d9f9b34ade132a3bf6e62b0bc0004964af
Kyzarok <mkl16@ic.ac.uk>
2018-03-19 14:03:28 +0000

    Stupid

--- src/AST/ast_varb_declarations.hpp
+++ src/AST/ast_varb_declarations.hpp
@@ -192,12 +192,12 @@ class CompoundStatement : public Node{
 				std::cerr<<"_____declCOMP2_____"<<std::endl;
 			}
 		}
-<<<<<<< HEAD
-		virtual void compile(std::ostream &dst) const override {
-=======
+
+
+
 		/*virtual void compile(std::ostream &dst) const override {
 			
->>>>>>> 8b3d4e1cf2bf7d4680f1f67742639b25a1e51f4e
+
 			if(dref!=NULL){
 				dref->compile(dst);
 			}
