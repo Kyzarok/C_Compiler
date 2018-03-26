d5aa6553a9f0b379c3d785bbe00a88a276bce2d5
Edward Pickup <pickup.edward@gmail.com>
2018-03-26 20:59:52 +0100

    Gave up on test bench. I tried using the test suite from last year as suggested by DT, but this did not work. Would have been nice to have actually done this together...

--- test_bench.sh
+++ test_bench.sh
@@ -1,17 +1,46 @@
 #!/bin/bash
 
-if [[ "$1" != "" ]] ; then
-    compiler="$1"
+if [[ -z "$1" ]]; then 
+    COMPILER=bin/c_compiler
 else
-    compiler="bin/c_compiler"
+    COMPILER=$1
 fi
 
-if [[ ! -f bin/c_compiler ]] ; then
-    >&2 echo "Warning : cannot find compiler at path ${compiler}. Stuff probably about to break."
-    have_compiler=1
-fi
+mkdir -p working
 
-input_dir="test_deliverable/test_cases"
-working = "tmp/test_out"
+for DRIVER in test_deliverable/testcases/*_driver.c ; do
+    NAME=$(basename $DRIVER _driver.c)
+    TESTCODE=test_deliverable/testcases/$NAME.c
+    
+    >&2 echo "Test case $NAME"
+    
+    # Compile driver with normal GCC
+    mips-linux-gnu-gcc -c $DRIVER -o working/${NAME}_driver.o 2> working/${NAME}_driver.compile.stderr
+    if [[ $? -ne 0 ]]; then
+        >&2 echo "ERROR : Couldn't compile driver program using GCC."
+        continue
+    fi
+    
+    # Compile test function with compiler under test to assembly
+    cat $TESTCODE | $COMPILER > working/$NAME.s  2> working/${NAME}.compile.stderr
+    if [[ $? -ne 0 ]]; then
+        >&2 echo "ERROR : Compiler returned error message."
+        continue
+    fi
+    
+    # Link driver object and assembly into executable
+    mips-linux-gnu-gcc -static working/${NAME}.s working/${NAME}_driver.o -o working/${NAME}.elf 2> working/${NAME}.link.stderr
+    if [[ $? -ne 0 ]]; then
+        >&2 echo "ERROR : Linker returned error message."
+        continue
+    fi
+    
+    # Run the actual executable
+    qemu-mips working/${NAME}.elf
+    if [[ $? -ne 0 ]]; then
+        >&2 echo "ERROR : Testcase returned $?, but expected 0."
+    fi
 
-mkdir -p ${working}
+    echo "pass"
+done
+
