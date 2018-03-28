779e8d81f630b6f8c71b992f7602ce94f9286ae8
Edward Pickup <pickup.edward@gmail.com>
2018-03-28 13:25:03 +0100

    Assessed feasibility of functional declaration with no body, started on final tweaks for submission

---
+++ examples/function_decl.s
@@ -0,0 +1,46 @@
+	.file	1 "function_decl.c"
+	.section .mdebug.abi32
+	.previous
+	.nan	legacy
+	.module	fp=xx
+	.module	nooddspreg
+	.abicalls
+	.text
+	.align	2
+	.globl	g
+	.set	nomips16
+	.set	nomicromips
+	.ent	g
+	.type	g, @function
+g:
+	.frame	$fp,32,$31		# vars= 0, regs= 2/0, args= 16, gp= 8
+	.mask	0xc0000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	addiu	$sp,$sp,-32
+	sw	$31,28($sp)
+	sw	$fp,24($sp)
+	move	$fp,$sp
+	lui	$28,%hi(__gnu_local_gp)
+	addiu	$28,$28,%lo(__gnu_local_gp)
+	.cprestore	16
+	lw	$2,%call16(f)($28)
+	move	$25,$2
+	.reloc	1f,R_MIPS_JALR,f
+1:	jalr	$25
+	nop
+
+	lw	$28,16($fp)
+	move	$sp,$fp
+	lw	$31,28($sp)
+	lw	$fp,24($sp)
+	addiu	$sp,$sp,32
+	j	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	g
+	.size	g, .-g
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.9) 5.4.0 20160609"
